9850496844e374836df6cffdd6d3b69450e61bcd
VascoMiguens <v_miguens@hotmail.com>
2023-04-15 18:35:35 +0100

    - fixed class syntax

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -5,35 +5,33 @@ const Footer = () => {
   return (
     <div className="footer">
       Copyright Arty Intelligence &copy; {new Date().getFullYear()}
-
-      <ul class="social-media-list">
-  <li>
-    <a href="https://www.instagram.com/your-username" target="_blank">
-      <i class="fab fa-instagram"></i>
-      Instagram
-    </a>
-  </li>
-  <li>
-    <a href="https://twitter.com/your-username" target="_blank">
-      <i class="fab fa-twitter"></i>
-      Twitter
-    </a>
-  </li>
-  <li>
-    <a href="https://www.facebook.com/your-username" target="_blank">
-      <i class="fab fa-facebook"></i>
-      Facebook
-    </a>
-  </li>
-  <li>
-    <a href="https://github.com/your-username" target="_blank">
-      <i class="fab fa-github"></i>
-      GitHub
-    </a>
-  </li>
-</ul>
-
-      </div>
+      <ul className="social-media-list">
+        <li>
+          <a href="https://www.instagram.com/your-username" target="_blank">
+            <i className="fab fa-instagram"></i>
+            Instagram
+          </a>
+        </li>
+        <li>
+          <a href="https://twitter.com/your-username" target="_blank">
+            <i className="fab fa-twitter"></i>
+            Twitter
+          </a>
+        </li>
+        <li>
+          <a href="https://www.facebook.com/your-username" target="_blank">
+            <i className="fab fa-facebook"></i>
+            Facebook
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/your-username" target="_blank">
+            <i className="fab fa-github"></i>
+            GitHub
+          </a>
+        </li>
+      </ul>
+    </div>
   );
 };
 
